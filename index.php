4124966344d12c885e977275d5eee36e2ce2f620
Luiz Sanches <luizgrsanches@gmail.com>
2013-10-18 23:01:24 -0300

    correcao de charset e separacao de folha de estilo

--- index.php
+++ index.php
@@ -8,215 +8,100 @@ $data = $server->loadState();
 $notification = $server->loadNotification();
 ?>
 <html>
-<head>
-<title>PagSeguro - Ambiente de Testes</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<style type="text/css">
-body, html {
-	padding: 0;
-	margin: 0;
-	height: 100%;
-	font-family: Arial, sans-serif;
-}
+	<head>
+		<title>PagSeguro - Ambiente de Testes</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="wrap">
+			<div id="main">
+				<div id="header">Ambiente de Testes PagSeguro <span id="header-github"><a href="https://github.com/bcarneiro/pagseguro-ambiente-testes">https://github.com/bcarneiro/pagseguro-ambiente-testes</a></span></div>
+				<div>
+				<?php if ($data) { ?>
+					<h3>Par‚metros Recebidos</h3>
+					<ul>
+					<?php
+					foreach($data as $key => $value) {
+						echo "<li>$key = '$value'</li>";
+					}
+					?>
+					<p class="warning">
+					<?php if (!$server->isDataConsistent()) { ?>
+					AtenÁ„o: os seguintes paramÍtros s„o obrigatÛrios mas n„o foram enviados: <?php echo $server->getMissingParameters(); ?>
+					<?php } ?>
+					</p>
+					</ul>
+					<?php if ($notification) { ?>
+					<h3 class="yellow">NotificaÁ„o enviada</h3>
+					<p>NotificaÁ„o enviada para <a href="<?php echo $server->getNotificationUrl(); ?>"><?php echo $server->getNotificationUrl(); ?></a>. Dados enviados:</p>
+					<ul>
+						<li>notificationCode: <?php echo $notification['notificationCode']; ?></li>
+						<li>notificationType: <?php echo $notification['notificationType']; ?></li>
+					</ul>
+					<p>Resposta do seu servidor:</p>
+					<div id="notification-response"><?php echo $notification['response']; ?></div>
+					<p>Para ler a notificaÁ„o envie um pedido GET para o endereÁo <a href="http://<?php echo $server->getCurrentHost(); ?>/notifications.php"><?php echo $server->getCurrentHost(); ?>/notifications.php</a> com os campos notificationCode, email (ignorado) e token (ignorado).</p>
+					<p>Exemplo: <a href="http://<?php echo $server->getCurrentHost(); ?>/notifications.php?notificationCode=<?php echo $notification['notificationCode']; ?>&email=teste@test.com&token=MEU-TOKEN-FALSO"><?php echo $server->getCurrentHost(); ?>/notifications.php?notificationCode=<?php echo $notification['notificationCode']; ?>&email=teste@test.com&token=MEU-TOKEN-FALSO</a></p><br>
+					<p>Dados da transaÁ„o:</p>
+					<ul>
+						<li>Status: <?php echo $notification['transactionStatus']; ?> (<?php echo $notification['transactionTextStatus']; ?>)</li>
+					</ul>
+					<?php } ?>
+					<h3>Enviar nova NotificaÁ„o</h3>
+					<p>Uma notificaÁ„o ser· enviada para o endereÁo <a href="<?php echo $server->getNotificationUrl(); ?>"><?php echo $server->getNotificationUrl(); ?></a>.</p>
+					<form method="post" action="notifications.php">
+						<p>
+							<label>Status:</label>
+							<select name="notificationStatus">
+								<option value="1">Aguardando pagamento</option>
+								<option value="2">Em an·lise</option>
+								<option value="3">Paga</option>
+								<option value="4">DisponÌvel</option>
+								<option value="5">Em disputa</option>
+								<option value="6">Devolvida</option>
+								<option value="7">Cancelada</option>
+							</select>
+						</p>
+						<p>
+							<label>Status:</label>
+							<select name="notificationType">
+								<option value="transaction">transaction</option>
+							</select>
+						</p>
+						<p>
+							<input type="submit" value="Enviar">
+						</p>
+					</form>
+					<h3 class="red">Apagar dados e notificaÁ„o</h3>
+					<p>Ir· apagar os dados guardados como os paramÍtros de ordem e notificaÁıes.</p>
+					<form method="post" action="wipe.php">
+						<p>
+							<input type="submit" value="Limpar todos os dados">
+						</p>
+					</form>
+				<?php } else { ?>
+					<h2>Tutorial - pagseguro-ambiente-testes</h2>
+					<p>Este software tem o objetivo de auxiliar o desenvolvedor a testar sua implementaÁ„o PagSeguro de forma pr·tica. … possÌvel enviar o seu carrinho de compras do PagSeguro e simular o sistema de notificaÁıes. Atualmente, o <a href="http://blogpagseguro.com.br/2012/05/testando-o-recebimento-de-notificacoes/">jeito atual recomendado pelo PagSeguro</a> È que se crie um vendedor falso e que ent„o que o desenvolvedor compre manualmente produtos, via boleto ou cart„o de crÈdito, o que se torna impr·tico na maioria das vezes. Este mÈtodo impede o desenvolvedor de usar sua m·quina local para testar o sistema de notificaÁıes, pois obviamente n„o È possÌvel enviar uma notificaÁ„o para um endereÁo local como 127.0.0.1. AlÈm disso n„o È possÌvel simular uma venda bem sucedida (a n„o ser que vocÍ realmente compre o produto). Com este sistema vocÍ conseguir· simular todos os tipos de notificaÁıes do PagSeguro de forma r·pida e pr·tica.</p>
+					<br>
+					<p>Para iniciar o ambiente de testes È necess·rio que vocÍ primeiro envie os dados do seu carrinho de compras. Portanto em vez de enviar os dados para o PagSeguro, vocÍ enviar· para est· pagina <a href="http://<?php echo $server->getCurrentHost(); ?>/checkout.php"><?php echo $server->getCurrentHost(); ?>/checkout.php</a>. Mais informaÁıes de quais dados s„o esperados e como funciona o carrinho de compras do PagSeguro vocÍ poder ler <a href="https://pagseguro.uol.com.br/desenvolvedor/carrinho_proprio.jhtml#rmcl">aqui</a>. N„o se esqueÁa tambÈm de alterar o arquivo PagSeguroServer.php e configurar as vari·veis $notification_domain e $notification_page (seu endereÁo para receber notificaÁıes).</p>
+					<br>
+					<p>Exemplo: <a href="http://<?php echo $server->getCurrentHost(); ?>/checkout.php?tipo=CP&moeda=BRL&email_cobranca=turm@test.com&item_id_1=1&item_descr_1=Computador%20bacana&item_quant_1=1&item_valor_1=100.00&item_id_2=2&item_descr_2=Mais%20um%20computador&item_quant_2=2&item_valor_2=150.00"><?php echo $server->getCurrentHost(); ?>/checkout.php?tipo=CP&moeda=BRL&ema...&item_valor_2=150.00</a></p><br>
+					<p>Tem alguma d˙vida, problema ou sugest„o? Quer contribuir? Estamos no Github, envie seu feedback para <a href="https://github.com/bcarneiro/pagseguro-ambiente-testes">pagseguro-ambiente-testes</a>!</p>
 
-#wrap {min-height: 100%;}
-
-#main {
-	overflow:auto;
-	padding-bottom: 40px;/* must be same height as the footer */
-}  
-
-#footer {
-	position: relative;
-	margin-top: -40px; /* negative value of footer height */
-	height: 30px;
-	clear:both;
-	
-	background-color: #3E4D3D;
-	color: white;
-	text-align: center;
-	padding-top: 10px;
-	text-shadow: 0px 1px 1px #444;
-	font-size: 0.9em;
-} 
-
-div#header {
-	background-color: #6BA81E;
-	line-height: 32px;
-	color: white;
-	text-shadow: 0px 1px 1px #444;
-	font-size: 1.2em;
-	padding: 10px;
-}
-
-span#header-github {
-	font-size: 0.8em;
-	padding-left: 15px;
-}
-
-span#header-github a {
-	color: #E4E4E4;
-	text-decoration: underline;
-}
-
-span#header-github a:hover {
-	text-decoration: none;
-}
-
-p.warning {
-	margin-top: 10px;
-	color: #811919;
-}
-
-h2, h3 {
-	font-family: Arial, sans-serif;
-	background-color: #C0EBBE;
-	font-weight: normal;
-	color: #343D33;
-	margin: 15px;
-	padding: 5px 0 5px 10px;
-	text-shadow: 0px 1px 1px white;
-}
-
-h3 {
-	margin: 15px 25px 15px 25px;
-	padding: 5px 0 5px 5px;
-	background-color: #C9E3C8;
-}
-
-h3.yellow {
-	background-color: #E3E0C8;
-}
-
-h3.red {
-	background-color: #E3CCC8;
-}
-
-div#notification-response {
-	display: block;
-	margin: 5px 45px 10px 45px;
-	border: 1px solid ccc;
-	padding: 5px;
-	background-color: whiteSmoke;
-	font-family: monospace;
-}
-
-p {
-	text-align: justify;
-}
-
-p {
-	margin: 0 30px;
-}
-
-input {
-	margin-top: 5px;
-}
-
-ul {
-	margin: 10px 15px;
-}
-
-a {
-	color: green;
-}
-
-a:hover {
-	text-decoration: none;
-}
-</style>
-</head>
-
-<body>
-	<div id="wrap">
-		<div id="main">
-			<div id="header">Ambiente de Testes PagSeguro <span id="header-github"><a href="https://github.com/bcarneiro/pagseguro-ambiente-testes">https://github.com/bcarneiro/pagseguro-ambiente-testes</a></span></div>
-			<div>
-			<?php if ($data) { ?>
-				<h3>Param√™tros Recebidos</h3>
-				<ul>
-				<?php
-				foreach($data as $key => $value) {
-					echo "<li>$key = '$value'</li>";
-				}
-				?>
-				<p class="warning">
-				<?php if (!$server->isDataConsistent()) { ?>
-				Aten√ß√£o: os seguintes param√™tros s√£o obrigat√≥rios mas n√£o foram enviados: <?php echo $server->getMissingParameters(); ?>
+					<h3>Links para documentaÁ„o</h3>
+					<ul>
+						<li><a href="https://github.com/bcarneiro/pagseguro-ambiente-testes">pagseguro-ambiente-testes</a></li>
+						<li><a href="https://pagseguro.uol.com.br/desenvolvedor/carrinho_proprio.jhtml#rmcl">Carrinho de Compras do PagSeguro</a></li>
+						<li><a href="https://pagseguro.uol.com.br/v2/guia-de-integracao/api-de-notificacoes.html">API de NotificaÁıes do PagSeguro</a></li>
+					</ul>
 				<?php } ?>
-				</p>
-				</ul>
-				<?php if ($notification) { ?>
-				<h3 class="yellow">Notifica√ß√£o enviada</h3>
-				<p>Notifica√ß√£o enviada para <a href="<?php echo $server->getNotificationUrl(); ?>"><?php echo $server->getNotificationUrl(); ?></a>. Dados enviados:</p>
-				<ul>
-					<li>notificationCode: <?php echo $notification['notificationCode']; ?></li>
-					<li>notificationType: <?php echo $notification['notificationType']; ?></li>
-				</ul>
-				<p>Resposta do seu servidor:</p>
-				<div id="notification-response"><?php echo htmlspecialchars($notification['response']); ?></div>
-				<p>Para ler a notifica√ß√£o envie um pedido GET para o endere√ßo <a href="http://<?php echo $server->getCurrentHost(); ?>/notifications.php"><?php echo $server->getCurrentHost(); ?>/notifications.php</a> com os campos notificationCode, email (ignorado) e token (ignorado).</p>
-				<p>Exemplo: <a href="http://<?php echo $server->getCurrentHost(); ?>/notifications.php?notificationCode=<?php echo $notification['notificationCode']; ?>&email=teste@test.com&token=MEU-TOKEN-FALSO"><?php echo $server->getCurrentHost(); ?>/notifications.php?notificationCode=<?php echo $notification['notificationCode']; ?>&email=teste@test.com&token=MEU-TOKEN-FALSO</a></p><br>
-				<p>Dados da transa√ß√£o:</p>
-				<ul>
-					<li>Status: <?php echo $notification['transactionStatus']; ?> (<?php echo $notification['transactionTextStatus']; ?>)</li>	
-				</ul>	
-				<?php } ?>
-				<h3>Enviar nova Notifica√ß√£o</h3>
-				<p>Uma notifica√ß√£o ser√° enviada para o endere√ßo <a href="<?php echo $server->getNotificationUrl(); ?>"><?php echo $server->getNotificationUrl(); ?></a>.</p>
-				<form method="post" action="notifications.php">
-					<p>
-						<label>Status:</label>
-						<select name="notificationStatus">
-							<option value="1">Aguardando pagamento</option>
-							<option value="2">Em an√°lise</option>
-							<option value="3">Paga</option>
-							<option value="4">Dispon√≠vel</option>
-							<option value="5">Em disputa</option>
-							<option value="6">Devolvida</option>
-							<option value="7">Cancelada</option>
-						</select>
-					</p>
-					<p>
-						<label>Status:</label>
-						<select name="notificationType">
-							<option value="1">transaction</option>
-						</select>
-					</p>
-					<p>
-						<input type="submit" value="Enviar">
-					</p>
-				</form>
-				<h3 class="red">Apagar dados e notifica√ß√£o</h3>
-				<p>Ir√° apagar os dados guardados como os param√™tros de ordem e notifica√ß√µes.</p>
-				<form method="post" action="wipe.php">
-					<p>
-						<input type="submit" value="Limpar todos os dados">
-					</p>
-				</form>
-			<?php } else { ?>
-				<h2>Tutorial - pagseguro-ambiente-testes</h2>
-				<p>Este software tem o objetivo de auxiliar o desenvolvedor a testar sua implementa√ß√£o PagSeguro de forma pr√°tica. √â poss√≠vel enviar o seu carrinho de compras do PagSeguro e simular o sistema de notifica√ß√µes. Atualmente, o <a href="http://blogpagseguro.com.br/2012/05/testando-o-recebimento-de-notificacoes/">jeito atual recomendado pelo PagSeguro</a> √© que se crie um vendedor falso e que ent√£o que o desenvolvedor compre manualmente produtos, via boleto ou cart√£o de cr√©dito, o que se torna impr√°tico na maioria das vezes. Este m√©todo impede o desenvolvedor de usar sua m√°quina local para testar o sistema de notifica√ß√µes, pois obviamente n√£o √© poss√≠vel enviar uma notifica√ß√£o para um endere√ßo local como 127.0.0.1. Al√©m disso n√£o √© poss√≠vel simular uma venda bem sucedida (a n√£o ser que voc√™ realmente compre o produto). Com este sistema voc√™ conseguir√° simular todos os tipos de notifica√ß√µes do PagSeguro de forma r√°pida e pr√°tica.</p>
-				<br>
-				<p>Para iniciar o ambiente de testes √© necess√°rio que voc√™ primeiro envie os dados do seu carrinho de compras. Portanto em vez de enviar os dados para o PagSeguro, voc√™ enviar√° para est√° pagina <a href="http://<?php echo $server->getCurrentHost(); ?>/checkout.php"><?php echo $server->getCurrentHost(); ?>/checkout.php</a>. Mais informa√ß√µes de quais dados s√£o esperados e como funciona o carrinho de compras do PagSeguro voc√™ poder ler <a href="https://pagseguro.uol.com.br/desenvolvedor/carrinho_proprio.jhtml#rmcl">aqui</a>. N√£o se esque√ßa tamb√©m de alterar o arquivo PagSeguroServer.php e configurar as vari√°veis $notification_domain e $notification_page (seu endere√ßo para receber notifica√ß√µes).</p>
-				<br>
-				<p>Exemplo: <a href="http://<?php echo $server->getCurrentHost(); ?>/checkout.php?tipo=CP&moeda=BRL&email_cobranca=turm@test.com&item_id_1=1&item_descr_1=Computador%20bacana&item_quant_1=1&item_valor_1=100.00&item_id_2=2&item_descr_2=Mais%20um%20computador&item_quant_2=2&item_valor_2=150.00"><?php echo $server->getCurrentHost(); ?>/checkout.php?tipo=CP&moeda=BRL&ema...&item_valor_2=150.00</a></p><br>
-				<p>Tem alguma d√∫vida, problema ou sugest√£o? Quer contribuir? Estamos no Github, envie seu feedback para <a href="https://github.com/bcarneiro/pagseguro-ambiente-testes">pagseguro-ambiente-testes</a>!</p>
-				
-				<h3>Links para documentac√£o</h3>
-				<ul>
-					<li><a href="https://github.com/bcarneiro/pagseguro-ambiente-testes">pagseguro-ambiente-testes</a></li>
-					<li><a href="https://pagseguro.uol.com.br/desenvolvedor/carrinho_proprio.jhtml#rmcl">Carrinho de Compras do PagSeguro</a></li>
-					<li><a href="https://pagseguro.uol.com.br/v2/guia-de-integracao/api-de-notificacoes.html">API de Notifica√ß√µes do PagSeguro</a></li>
-				</ul>
-			<?php } ?>
+				</div>
 			</div>
 		</div>
-	</div>
 
-	<div id="footer">
-	<span>Este software √© gratuito e n√£o est√° associado com o PagSeguro. PagSeguro √© uma marca registrada da empresa UOL. Este ambiente de testes n√£o √© afiliado com a UOL e portanto n√£o √© um produto oficial do PagSeguro.</span>
-	</div>
-</body>
-
+		<div id="footer">
+			<span>Este software È gratuito e n„o est· associado com o PagSeguro. PagSeguro È uma marca registrada da empresa UOL. Este ambiente de testes n„o È afiliado com a UOL e portanto n„o È um produto oficial do PagSeguro.</span>
+		</div>
+	</body>
 </html>
